5651dba9c087cbaad60ccc0a85df7bb0b4176289
cusen11 <cusenbonchen@gmail.com>
2022-02-20 21:31:11 +0700

    ád

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -71,5 +71,14 @@
     $('.news-prev').click(function() {
         owlNews.trigger('prev.owl.carousel');
     })
-    
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+    wow.init();
 }())
